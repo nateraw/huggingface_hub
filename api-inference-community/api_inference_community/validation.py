7a1722570a2b170f9345072eef9bd0c9a2d8c97e
Nicolas Patry <patry.nicolas@protonmail.com>
2021-06-09 16:04:58 +0200

    Add `audio-to-audio` and remove `audio-source-separation` to this package.

--- api-inference-community/api_inference_community/validation.py
+++ api-inference-community/api_inference_community/validation.py
@@ -197,6 +197,7 @@ def normalize_payload(
 ) -> Tuple[Any, Dict]:
     if task in {
         "automatic-speech-recognition",
+        "audio-source-separation",
         "audio-to-audio",
     }:
         if sampling_rate is None:
